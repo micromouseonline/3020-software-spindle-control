319ae5e9f8ae21732f048da0d18affb03e2c9418
Eric Poulsen <eric@zyxod.com>
2018-03-13 15:10:02 -0700

    added pot input, digipot output

--- fw/src/main.ino
+++ fw/src/main.ino
@@ -1,6 +1,14 @@
 
 #include <elapsedMillis.h>
-#define PIN 2
+#include <SPI.h>
+
+#define PWM_IN_PIN 2
+#define POT_IN_PIN 0
+#define CS_PIN 10
+#define SCK_PIN 15
+#define MOSI_PIN 16
+
+SPISettings spiSet(5000000, MSBFIRST, SPI_MODE0);
 
 volatile int onTime = 0;
 volatile int offTime = 0;
@@ -10,14 +18,15 @@ elapsedMillis printTimer;
 
 #define AVG_LEN 15
 
-float pcts[AVG_LEN];
+float pwmPcts[AVG_LEN];
 float outputPct;
-float pct;
+float pwmPct;
+float potPct;
 
 void interrupt () {
     long now = micros();
     long elapsed = lastTime - now;
-    bool state = digitalRead(PIN);
+    bool state = digitalRead(PWM_IN_PIN);
 
     if (state) {
         offTime = elapsed;
@@ -30,15 +39,43 @@ void interrupt () {
 
 void setup() {
 
-    pinMode(PIN, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(PIN), interrupt, CHANGE);
+    pinMode(PWM_IN_PIN, INPUT_PULLUP);
+    pinMode(CS_PIN, OUTPUT);
+
+    SPI.begin();
+    setOutput(0);
+
+    attachInterrupt(digitalPinToInterrupt(PWM_IN_PIN), interrupt, CHANGE);
     Serial.begin(115200);
     
     for (int i = 0; i < AVG_LEN; ++i) {
-        pcts[i] = 0;
+        pwmPcts[i] = 0;
     }
-    pct = 0;
+
+    pwmPct = 0;
+    potPct = 0;
     outputPct = 0;
+}
+
+void setOutput(float pct) {
+	
+
+    // We're emulating a 5K pot with a
+    // 10K digi-pot, so we're
+    // only going to use 1/2 the rang
+
+    uint8_t value = 0x80 + pct * 0x7f;
+
+        Serial.print("value: ");
+        Serial.print(value); 
+        Serial.println();
+
+    SPI.beginTransaction(spiSet);
+    digitalWrite(CS_PIN, LOW);
+    SPI.transfer(0x11); // Write potentiometer 0
+    SPI.transfer(value); // Set wiper position
+    digitalWrite(CS_PIN, HIGH);
+    SPI.endTransaction();
 }
 
 void loop() {
@@ -46,12 +83,12 @@ void loop() {
     static int pctIndex = 0;
     long curOnTime, curOffTime;
     bool flag = false;
-    float cur = digitalRead(PIN);
+    float cur = digitalRead(PWM_IN_PIN);
 
     noInterrupts();
     long now = micros();
     long elapsed = now - lastTime;
-    if (elapsed < 100000) {
+    if (elapsed < 100000) { // 100ms
         curOnTime = onTime;
         curOffTime = offTime;
         flag = true;
@@ -63,7 +100,7 @@ void loop() {
     if (flag && curOnTime && curOffTime) {
         cur = (float)curOnTime / (curOffTime + curOnTime);
     }
-    pcts[pctIndex++] = cur;
+    pwmPcts[pctIndex++] = cur;
 
     if (pctIndex >= AVG_LEN) {
         pctIndex = 0;
@@ -71,13 +108,15 @@ void loop() {
 
 
     if (outputTimer > 50) {
-        pct = 0;
-        for (int i = 0; i < AVG_LEN; ++i) {
-            pct += pcts[i];
-        }
-        pct /= AVG_LEN;
+        pwmPct = 0;
 
-        float delta = outputPct - pct;
+        for (int i = 0; i < AVG_LEN; ++i) {
+            pwmPct += pwmPcts[i];
+        }
+        pwmPct /= AVG_LEN;
+	potPct = analogRead(POT_IN_PIN) / 1023.0f;
+
+        float delta = outputPct - min(pwmPct, potPct);
         float aDelta = fabs(delta);
         if (delta) {
             // No more than 2% per 50ms
@@ -89,17 +128,18 @@ void loop() {
             }
         }
         outputTimer = 0;
-
+	setOutput(outputPct);
     }
+
     if (printTimer > 75) {
-        Serial.print(pct*100); 
+        Serial.print("PWM in: ");
+        Serial.print(pwmPct*100); 
         Serial.print("%");
+        Serial.print(" POT in: ");
+        Serial.print(potPct*100); 
         Serial.print("  out: ");
         Serial.print(outputPct * 100); 
         Serial.println();
         printTimer = 0;
     }
-
-
-
 }
